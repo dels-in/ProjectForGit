377295082f21b2122caffb8b0bc4d4a838f2604e
dels-in <chernenko3954@mail.ru>
2023-09-05 11:21:38 +0300

    completed task4

--- ProjectForGit/Program.cs
+++ ProjectForGit/Program.cs
@@ -1,7 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
 //изменение для подзадачи 1
 
 //изменение для подзадачи 2
 
 //изменение для подзадачи 3
+
+//изменение для подзадачи 4
