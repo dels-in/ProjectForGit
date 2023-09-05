3cebec4f283b8b3ba4f3f5e4a0d2fb97958ac9fb
dels-in <chernenko3954@mail.ru>
2023-09-05 11:00:44 +0300

    completed task2

---
+++ ProjectForGit/BooksStorage.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace ProjectForGit
+{
+	public class BookStorage
+	{
+		public BookStorage()
+		{
+		}
+	}
+}
+
--- ProjectForGit/Program.cs
+++ ProjectForGit/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 //изменение для подзадачи 1
+//изменение для подзадачи 2
